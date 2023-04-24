09170687960a7cdd13d8f0057c80202a08d78adf
barryjung <wjdgusrbs5@gmail.com>
2023-04-24 22:21:35 +0900

    Update signup

--- user/models.py
+++ user/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class UserModel(AbstractUser):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=30, blank=True)
+    genders = (("M", "male"), ("F", "female"),)
+    gender = models.CharField(choices=genders, max_length=1, blank=True)
+    age = models.IntegerField(blank=True, null=True)
+    introduction = models.TextField(blank=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
---
+++ user/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from user.models import UserModel
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserModel
+        fields = "__all__"
+
+    def create(self, validated_data):
+        user = super().create(validated_data)
+        password = user.password
+        user.set_password(password)
+        user.save()
+        return user
+
+    def update(self, validated_data):
+        user = super().create(validated_data)
+        password = user.password
+        user.set_password(password)
+        user.save()
+        return user
